d4cb9f29e76d26292e021468668bb84e4dbd1226
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-21 01:51:08 +0300

    love calculator

---
+++ Javascript/love calculator.js
@@ -0,0 +1,8 @@
+var name1 = prompt("What is your name? ");
+var name2 = prompt("What is your lovers name? ");
+
+var n = Math.random(n);
+n = n * 100;
+
+n = Math.floor(n);
+console.log( name1 + " + " + name2 + " your love percentage is " + n + "%");
