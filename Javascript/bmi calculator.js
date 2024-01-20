25e2480d0d69ece4d42a737c55324067348dc158
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-21 01:18:10 +0300

    bmi calculator

--- Javascript/bmi calculator.js
+++ Javascript/bmi calculator.js
@@ -1,7 +1,7 @@
 //Create your function below this line.
 //The first parameter should be the weight and the second should be the height.
 function bmiCalculator(weight,height){
-    var bmi = Math.floor(weight / height);
+    var bmi = Math.floor(weight / (height*height));
     return bmi;
 }
 
