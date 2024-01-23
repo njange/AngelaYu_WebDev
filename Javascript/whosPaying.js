800da7f50dd28ea14769101aff82b944e66028f1
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-24 02:51:10 +0300

    Who is buying lunch code challange

--- Javascript/whosPaying.js
+++ Javascript/whosPaying.js
@@ -1,3 +1,11 @@
-function whosPaying() {
+names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+
+function whosPaying(names) {
+    var numberOfPeople = names.length;
+    var randomPersonPosition = math.floor(Math.random() * numberOfPeople)
+    var randomPerson = names[randomPersonPosition];
+    return randomPerson + "is going to buy lunch today!";
     
 }
+
+whosPaying();
