1f6023321e19c224185c8450f8c0976dab8eaba2
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-24 00:54:40 +0300

    guest list challange complete

--- Javascript/arrays.js
+++ Javascript/arrays.js
@@ -1,4 +1,9 @@
 var guestList = ["Angela", "Jack","Pam","Lara","Jason"];
 
 console.log(guestList[1]);
-guestList.includes("Angela");
+
+ var name = prompt("What is your name? ")
+
+if (guestList.includes(name)){
+    console.log("Welcome " + name)}
+else {console.log("Not found.")}
