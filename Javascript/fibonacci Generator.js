a2b2386891a4b90c727fba6d875b1dae331610b6
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-30 07:06:16 +0300

    fibonacci sequence trial

--- Javascript/fibonacci Generator.js
+++ Javascript/fibonacci Generator.js
@@ -1,1 +1,20 @@
-function 
+function fibonacciGenerator(n) {
+    var output = [];
+    if (n===1) {
+        output = [0];
+    }
+    else if (n===2) {
+        output = [0,1];
+    }
+    else {
+        output = [0,1];
+        output.push(output[output.length-2] + output[output.length-1]);
+        if (n===output.length) {
+            
+        }
+        else {
+            
+        }
+    }
+    return output
+}
