f3836e442e4aa0db9cec884e67c76243aa44473d
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-13 23:05:44 +0300

    event listener for loops

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,4 +1,10 @@
-document.querySelector("button").addEventListener("click", handleClick)
-function handleClick(){
-    alert("I got clicked")
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  
+
+document.querySelector("button").addEventListener("click", function (){
+    alert("I got clicked");
+
+});
 }
