bac1ee8f23d4ebae14d05d53a2cd0c75a8ea0a28
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-14 23:40:53 +0300

    audio start

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,7 +6,10 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 document.querySelector("button").addEventListener("click", function (){
     alert("I got clicked");
 
+    var audio = new Audio("audio_file.mp3");
+    audio.play();
+
 });
 }
 
-
+ 
