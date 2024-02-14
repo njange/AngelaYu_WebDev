63e2817ea42bced5237b32ec0c98bd00e360b7dd
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-14 23:41:37 +0300

    tom1

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,7 +6,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 document.querySelector("button").addEventListener("click", function (){
     alert("I got clicked");
 
-    var audio = new Audio("audio_file.mp3");
+    var audio = new Audio("sounds/tom-1.mp3");
     audio.play();
 
 });
