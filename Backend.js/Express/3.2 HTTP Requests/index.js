6e1886d83c907a908f65d79f96bf48dab4873606
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-12 00:16:43 +0300

    sending request

--- Backend.js/Express/3.2 HTTP Requests/index.js
+++ Backend.js/Express/3.2 HTTP Requests/index.js
@@ -2,6 +2,12 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+
+app.get('/', (req, res) => {
+    res.send(req);
+});
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
