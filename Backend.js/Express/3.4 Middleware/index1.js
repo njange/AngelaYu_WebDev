d700fbc5ab61af387921f03a2293dd7ae0740f88
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-28 00:11:44 +0300

    made post request

--- Backend.js/Express/3.4 Middleware/index1.js
+++ Backend.js/Express/3.4 Middleware/index1.js
@@ -1,14 +1,23 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  console.log(__dirname + "/public/index.html"); //listening to the path
+
   res.sendFile(__dirname + "/public/index.html"); //path to the file index.html
+});
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+
 });
 
 app.listen(port, () => {
