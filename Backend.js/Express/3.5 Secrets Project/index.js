d710ca394be20de65f4289e8379b0d9f9d45b14a
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-02 23:57:26 +0300

    set password and ports

--- Backend.js/Express/3.5 Secrets Project/index.js
+++ Backend.js/Express/3.5 Secrets Project/index.js
@@ -3,3 +3,22 @@ import bodyParser from 'body-parser';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body['password'];
+  if (password === 'ILoveProgramming') {
+    userIsAuthorised = true;
+  }
+  next();
+}
+
+app.use(passwordCheck);
+
