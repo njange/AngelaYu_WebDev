73f1e05920e4962b342c74cf6f2fddd7d9633645
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-02 23:55:48 +0300

    imported modules

--- Backend.js/Express/3.5 Secrets Project/index.js
+++ Backend.js/Express/3.5 Secrets Project/index.js
@@ -1,4 +1,5 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-n
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
