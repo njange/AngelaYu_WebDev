dbe7673424c17cdd2cb021ecd71b01442f64a8e9
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-06 00:15:12 +0300

    render

--- Backend.js/4.0 EJS/index.js
+++ Backend.js/4.0 EJS/index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.render('index.ejs', {dayType: "a weekday", advice: "Take it easy"});
+    }); 
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
     });
