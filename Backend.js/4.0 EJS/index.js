8795a3c3fb37bf7e4576f6ed4f023dfe3f6ae9d2
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-06 00:25:21 +0300

    conditions

--- Backend.js/4.0 EJS/index.js
+++ Backend.js/4.0 EJS/index.js
@@ -4,8 +4,20 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+    const today = new Date();
+    const day = today.getDay();
+
+    let type = "a weekday";
+    let advice = "Work hard";
+
+    if (day === 0 || day === 6) {
+        type = "a weekend";
+        advice = "Party hard";
+    }
+
     res.render('index.ejs', {dayType: "a weekday", advice: "Take it easy"});
     }); 
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
